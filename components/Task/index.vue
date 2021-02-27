d6e4b99071ba5c129079d32af3641661f4d8b616
Jolfr <thomas.jack.carroll@gmail.com>
2021-02-27 12:18:32 -0500

    added basic tailwinds def for task

--- components/Task/index.vue
+++ components/Task/index.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="list-item">
-    <input type="text" readonly :value="task.title" />
+  <div
+    class="border-1 border-gray-500 rounded-full shadow-md hover:shadow-lg inline-flex justify-between overflow-x-auto"
+    :class="task.state"
+  >
+    <label class="checkbox inline px-2">
+      <input type="checkbox" :checked="isChecked" name="archived" />
+    </label>
+    <div class="title inline px-2">
+      {{ task.title }}
+    </div>
+    <label class="checkbox inline px-2">
+      <input type="checkbox" :checked="isChecked" name="pinned" />
+    </label>
   </div>
 </template>
 
@@ -13,6 +24,11 @@ export default {
       required: true,
       default: () => ({ id: '', state: '', title: '' }),
       validator: (task) => ['id', 'state', 'title'].every((key) => key in task),
+    },
+  },
+  computed: {
+    isChecked() {
+      return this.task.state === 'TASK_ARCHIVED'
     },
   },
 }
